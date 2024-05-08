f5695fc7786494a8c80547e4f3011bdbdd864d6d
gaeun <geun5744@gmail.com>
2024-05-08 14:46:22 +0900

    feat: dao 구현

---
+++ data/src/main/java/woowacourse/movie/dao/ScreeningMovieDao.kt
@@ -0,0 +1,28 @@
+package woowacourse.movie.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import woowacourse.movie.entity.ScreeningMovie
+
+@Dao
+interface ScreeningMovieDao {
+    @Query("SELECT * FROM screening_movie")
+    fun getAll(): List<ScreeningMovie>
+
+    @Insert
+    fun insertAll(vararg screeningMovies: ScreeningMovie)
+
+    @Delete
+    fun delete(screeningMovie: ScreeningMovie)
+
+    @Query("SELECT * FROM screening_movie WHERE id = :screenMovieId")
+    fun getScreenMovieById(screenMovieId: Long): ScreeningMovie
+
+    @Query("SELECT * FROM screening_movie WHERE movie_id = :movieId AND theater_id = :theaterId")
+    fun getByMovieIdAndTheaterId(
+        movieId: Long,
+        theaterId: Long,
+    ): List<ScreeningMovie>
+}
