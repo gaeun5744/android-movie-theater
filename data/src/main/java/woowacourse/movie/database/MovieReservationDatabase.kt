074005e0660805dc7d9110a11f74fe5600f967cd
gaeun <geun5744@gmail.com>
2024-05-08 14:58:25 +0900

    feat: typeConverter 추가

--- data/src/main/java/woowacourse/movie/database/MovieReservationDatabase.kt
+++ data/src/main/java/woowacourse/movie/database/MovieReservationDatabase.kt
@@ -1,10 +1,15 @@
 package woowacourse.movie.database
 
 import androidx.room.Database
+import androidx.room.TypeConverters
+import woowacourse.movie.converter.DurationConverters
+import woowacourse.movie.converter.LocalDateTimeConverters
+import woowacourse.movie.converter.ReservationSeatsConverters
 import woowacourse.movie.dao.MovieReservationDao
 import woowacourse.movie.entity.MovieReservation
 
 @Database(entities = [MovieReservation::class], version = 1)
+@TypeConverters(value = [LocalDateTimeConverters::class, DurationConverters::class, ReservationSeatsConverters::class])
 abstract class MovieReservationDatabase {
     abstract fun movieReservationDao(): MovieReservationDao
 }
--- data/src/main/java/woowacourse/movie/database/ScreeningMovieDatabase.kt
+++ data/src/main/java/woowacourse/movie/database/ScreeningMovieDatabase.kt
@@ -1,10 +1,13 @@
 package woowacourse.movie.database
 
 import androidx.room.Database
+import androidx.room.TypeConverters
+import woowacourse.movie.converter.ScreenDateTimeConverters
 import woowacourse.movie.dao.ScreeningMovieDao
 import woowacourse.movie.entity.ScreeningMovie
 
 @Database(entities = [ScreeningMovie::class], version = 1)
+@TypeConverters(value = [ScreenDateTimeConverters::class])
 abstract class ScreeningMovieDatabase {
     abstract fun screeningMovieDao(): ScreeningMovieDao
 }
